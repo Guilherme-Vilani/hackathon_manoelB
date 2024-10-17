94983f4c8724c9bad6ddf99ebf4350891d437fb9
Guilherme-Vilani <gui.vilani79@gmail.com>
2024-10-17 13:38:15 -0400

    primeiro commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from fastapi import Body, FastAPI, HTTPException
+from pydantic import BaseModel
+
+class Atendimento(BaseModel):
+    cliente: str
+    telefone: int
+    descricao_equipamento: str
+    tecnico_responsavel: str
+
+
+atendimentos = []
+
+# Criar a instância do FastAPI
+app = FastAPI()
+
+@app.get("/consulta-atendimentos")
+async def consulta_atendimentos():
+    return atendimentos
+
+@app.get("/consulta-atendimento-telefone")
+async def consulta_atendimentos_telefone(telefone: int):
+    # Filtrar os atendimentos com o número de telefone correspondente
+    resultado = [
+        atendimento for atendimento 
+        in atendimentos 
+        if atendimento.telefone == telefone
+        ]
+    return resultado
+
+
+@app.get("/consulta-atendimento-nome")
+async def consulta_atendimentos_nome(nome: str):
+    # Filtrar os atendimentos com o número de telefone correspondente
+    resultado = [
+        atendimento for atendimento 
+        in atendimentos 
+        if atendimento.cliente == nome
+    ]
+    return resultado
+
+
+@app.put("/cadastra-atendimento")
+async def cadastra_atendimento(body: Atendimento):
+    
+    atendimentos.append(body)
+    
+    return body
+
+# Rota para alterar o atendimento
+@app.post("/altera-registro-atendimento")
+async def atualiza_atendimento(telefone: int, body: Atendimento):
+    # Verificar se o atendimento com o telefone existe
+    for i, atendimento in enumerate(atendimentos):
+        if atendimento.telefone == telefone:
+            # Atualizar o registro encontrado
+            atendimentos[i] = body
+            return {"message": "Atendimento atualizado com sucesso", "atendimento": body}
+    
+    # Se não encontrar o atendimento, lançar exceção
+    raise HTTPException(status_code=404, detail="Atendimento não encontrado")
